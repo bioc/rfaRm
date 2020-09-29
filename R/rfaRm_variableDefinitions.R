add394c48885fd63aede77781734372cf8fd5ce2
Lara Selles <laraselles@dyn3205-118.wlan.ic.ac.uk>
2020-09-29 14:28:56 +0100

    v 1.1.4 fixed issue caused by OpenSSL 1.1.1 bug

--- R/rfaRm_variableDefinitions.R
+++ R/rfaRm_variableDefinitions.R
@@ -1,17 +1,17 @@
 ## URL for keyword-based searches of the Rfam database
-rfamEbiApiURL <- 'https://www.ebi.ac.uk/ebisearch/ws/rest/rfam'
+rfamEbiApiURL <- 'http://www.ebi.ac.uk/ebisearch/ws/rest/rfam'
 
 ## Base URL used for most queries to the Rfam database
 rfamApiBaseURL <- 'http://rfam.org/family/'
 
 ## Base URL used for sequence-based searches of the Rfam database
-rfamApiSequenceSearchURL <- 'https://rfam.org/search/sequence'
+rfamApiSequenceSearchURL <- 'http://rfam.org/search/sequence'
 
 ## URL used to retrieve the list of clans defined in the Rfam database
-rfamClansListURL <- 'https://rfam.xfam.org/clans'
+rfamClansListURL <- 'http://rfam.xfam.org/clans'
 
 ## Base URL used to retrieve the Rfam families belonging to a specific clan
-rfamClanLookUpURL <- 'https://rfam.xfam.org/clan/'
+rfamClanLookUpURL <- 'http://rfam.xfam.org/clan/'
 
 ## Named list where the name of each element is an Rfam clan, and each element
 ## is a character vector with the Rfam families of the corresponding clan
